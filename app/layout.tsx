f960a27f70b640542a15ea017e7ac75cafda1fbd
MK MUZAKKIR <muzakkir7977@gmail.com>
2025-05-06 01:47:20 -0400

    sdf

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,19 +3,19 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/toaster";
-import Head from "next/head";
+
 const inter = Inter({ subsets: ["latin"] });
 
-// export const metadata: Metadata = {
-//   title: "Scratch & Reveal Your Exclusive Coupon | MedZeal",
-//   description: "Scratch the card to reveal your exclusive MedZeal discount coupon for 2025",
-//   openGraph: {
-//     title: "Scratch to Reveal Your Coupon!",
-//     description: "Unlock your exclusive offer with Medzeal!",
-//     images: ["https://raw.githubusercontent.com/infisparks/images/refs/heads/main/gift.png"],
-//     type: "website",
-//   },
-// };
+export const metadata: Metadata = {
+  title: "Scratch & Reveal Your Exclusive Coupon | MedZeal",
+  description: "Scratch the card to reveal your exclusive MedZeal discount coupon for 2025",
+  openGraph: {
+    title: "Scratch to Reveal Your Coupon!",
+    description: "Unlock your exclusive offer with Medzeal!",
+    images: ["https://raw.githubusercontent.com/infisparks/images/refs/heads/main/gift.png"],
+    type: "website",
+  },
+};
 
 export default function RootLayout({
   children,
@@ -24,34 +24,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
-       <Head>
-        {/* Open Graph / WhatsApp Share */}
-
-        <link rel="icon" type="image/svg+xml" href="/favicon.ico" />
-        <meta property="og:title" content="Scratch to Reveal Your Coupons" />
-        <meta
-          property="og:description"
-          content="Unlock your exclusive offer with Medzeal!"
-        />
-        <meta
-          property="og:image"
-          content="https://raw.githubusercontent.com/infisparks/images/refs/heads/main/gift.png"
-        />
-         <meta property="og:url" content="https://coupon.medzeal.in" />
-        <meta property="og:type" content="website" />
-
-        {/* Twitter Card (optional) */}
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="Scratch to Reveal Your Coupon!" />
-        <meta
-          name="twitter:description"
-          content="Unlock your exclusive offer with Medzeal!"
-        />
-        <meta
-          name="twitter:image"
-          content="https://raw.githubusercontent.com/infisparks/images/refs/heads/main/gift.png"
-        />
-      </Head>
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </head>
       <body className={inter.className}>
         <ThemeProvider
           attribute="class"
